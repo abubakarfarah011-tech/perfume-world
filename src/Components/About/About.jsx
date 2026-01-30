2d4689882b384c4b6e94a8ee1910cbbe7cad2582
abubakar <abubakarfarah011@gmail.com>
2026-01-30 21:17:09 +0300

    updated footer and about page

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,23 @@
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Our Perfume-World</h1>
+
+      <p> We sell quality perfumes for daily use and special occasions. </p>
+
+      <div className="about-card">
+        <h3>Our Mission</h3>
+        <p>To provide affordable and long-lasting fragrances.</p>
+      </div>
+
+      <div className="about-card">
+        <h3>Our Vision</h3>
+        <p>To become a trusted online perfume shop.</p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h3>Perfume-World</h3>
+      <p>The Best Fragrance For You</p>
+
+      <div className="social-links">
+        <a href="https://facebook.com">Facebook</a>
+        <a href="https://instagram.com">Instagram</a>
+        <a href="https://twitter.com">Twitter</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
